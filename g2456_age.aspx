37622eb2581f12a31bc7470a0675ae50c667d362
vebzical <vebzical@hotmail.com>
2013-09-19 10:01:21 +0300

    Mestarin kalenteri
    Masterpage ja kalenteri missio

---
+++ g2456_age.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="g2456_age.aspx.cs" Inherits="g2456_age" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <br/><asp:Label ID="texti" runat="server" Text="Label"></asp:Label><br/>
+    <asp:Label ID="texti2" runat="server" Text="Label"></asp:Label><br />
+    <asp:Label ID="texti3" runat="server" Text="Label"></asp:Label>
+    <br />
+    <asp:Button ID="Button1" runat="server" OnClick="button1Painettu" Text="&lt;&lt;&lt; Vuosi" />
+    <asp:Button ID="Button2" runat="server" OnClick="button2Painettu" Text="Vuosi &gt;&gt;&gt;" />
+    <br />
+    <asp:Calendar ID="Calendar1" runat="server" Width="244px" OnInit="tanaanOn" OnSelectionChanged="paivaaVaihdettu">
+        <OtherMonthDayStyle BackColor="#999999" />
+        <TodayDayStyle BackColor="#0099FF" />
+        <WeekendDayStyle BackColor="#33CC33" BorderColor="#009933" BorderStyle="Solid" BorderWidth="1px" ForeColor="#003300" />
+    </asp:Calendar>
+</asp:Content>
+
---
+++ g2456_age.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class g2456_age : System.Web.UI.Page
+{
+    string currentDate; 
+    string selectedDate;
+
+    DateTime tamaPaiva;
+    DateTime valittuPaiva;
+    TimeSpan paivienErotus;
+    int paivina;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        texti2.Text = "Valitse haluamasi päivä.";
+        texti3.Text = null;
+    }
+
+    protected void tanaanOn(object sender, EventArgs e)
+    {
+        Calendar1.SelectedDate = DateTime.Today;
+        Calendar1.VisibleDate = DateTime.Today;
+
+        currentDate = DateTime.Today.GetDateTimeFormats('d')[0];
+        texti.Text = "Tänään on: "+currentDate;
+    }
+
+    protected void paivaaVaihdettu(object sender, EventArgs e)
+    {
+        texti2.Text = null;
+        texti3.Text = null;
+
+        selectedDate = Calendar1.SelectedDate.GetDateTimeFormats('d')[0];
+        texti2.Text = "Valitse haluamasi päivä "+selectedDate;
+
+        valittuPaiva = Calendar1.SelectedDate;
+        tamaPaiva = DateTime.Today;
+
+        paivienErotus = tamaPaiva - valittuPaiva;
+        paivina = paivienErotus.Days;
+
+        texti3.Text = "Valitun päivän ja tämän päivän erotus: " + paivina.ToString();
+    }
+
+    protected void button1Painettu(object sender, EventArgs e) 
+    {
+        Calendar1.VisibleDate = Calendar1.VisibleDate.AddYears(-1);
+    }
+
+    protected void button2Painettu(object sender, EventArgs e)
+    {
+        Calendar1.VisibleDate = Calendar1.VisibleDate.AddYears(1);
+    }
+}
