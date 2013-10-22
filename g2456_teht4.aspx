a194d81c9b4940352aaa92772d58f0d27f8b8721
vebzical <vebzical@hotmail.com>
2013-10-22 15:48:42 +0300

    Tehtava 4 tehty
    Tein tehtävän 4.

---
+++ g2456_teht4.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/teht3.master" AutoEventWireup="true" CodeFile="g2456_teht4.aspx.cs" Inherits="g2456_teht4" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:TextBox ID="TextBox1" runat="server" ToolTip="Hae merkin tai mallin mukaan" ></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Hae" onClick="Button1_Click"/>
+    <asp:ListBox ID="ListBox1" runat="server" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" AutoPostBack="True" 
+        style="float:left;" ViewStateMode="Enabled" EnableViewState="true"  ></asp:ListBox> 
+
+    <asp:ListView ID="ListView1" runat="server" onSorting="ListView1_Sorting"> 
+    <LayoutTemplate>
+        <table cellpadding="2" width="640px" border="1" runat="server" style="margin:10px 10px 10px 10px;" id="tblCars">
+            <tr id="Tr1" runat="server">
+             <th id="Th1" runat="server">Merkki</th>
+             <th id="Th2" runat="server">Malli</th>
+             <th id="Th3" runat="server">Rekkari</th>
+             <th id="Th4" runat="server">Vuosi Malli</th>
+             <th id="Th5" runat="server">Myynti Hinta <asp:LinkButton ID="sortByPrice" runat="server" 
+                 CommandName="Sort" CommandArgument="MyyntiHinta" >Sort by price</asp:LinkButton></th>
+             <th id="Th6" runat="server">Sisäänosto Hinta</th>
+            </tr>
+            <tr runat="server" id="itemPlaceholder" />
+        </table>
+    </LayoutTemplate>
+
+    <ItemTemplate>
+    <tr id="Tr2" runat="server">
+        <td>
+            <asp:Label ID="lblMerkki" runat="Server" Text='<%#Eval("Merkki") %>' />
+        </td>
+        <td>
+            <asp:Label ID="lblMalli" runat="Server" Text='<%#Eval("Malli") %>' />
+        </td>
+        <td >
+            <asp:Label ID="lblRekkari" runat="Server" Text='<%#Eval("Rekkari") %>' />
+        </td>
+        <td >
+            <asp:Label ID="lblVm" runat="Server" Text='<%#Eval("Vm") %>' />
+        </td>
+        <td >
+            <asp:Label ID="lblMyynti" runat="Server" Text='<%#Eval("MyyntiHinta") %>' />
+        </td>
+        <td >
+            <asp:Label ID="lblSOstoHinta" runat="Server" Text='<%#Eval("SOstoHinta") %>' />
+        </td>
+    </tr>
+    </ItemTemplate>
+
+    </asp:ListView>
+    <div runat="server" id="er"></div> 
+
+
+</asp:Content>
+
---
+++ g2456_teht4.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public partial class g2456_teht4 : System.Web.UI.Page
+{
+    public List<Auto> Autot { get; set; }
+    public string SortExpression { 
+        get { 
+            return (string)ViewState["SortExpression"]; 
+        } 
+        set { 
+            ViewState["SortExpression"] = value; 
+        }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack) 
+        {
+            initMyStuff();
+        }
+        else
+        {
+            Autot=(List<Auto>)ViewState["AutotLista"];
+        }
+    }
+
+    private void initMyStuff()
+    {
+        try
+        {
+            AutoLista autot = new AutoLista();
+            Serialisointi.DeSerialisoiXml(Server.MapPath("~/App_Data/WanhatAutot.xml"), ref autot);
+            Random random = new Random();
+            List<Auto> nelja = new List<Auto>();
+
+            while (nelja.Count < 4)
+            {
+                int luku = random.Next(0, autot.Autot.Count - 1);
+                if (!nelja.Contains(autot.Autot.ElementAt(luku)))
+                {
+                    nelja.Add(autot.Autot.ElementAt(luku));
+                }
+            }
+            Autot = autot.Autot;
+            ViewState["AutoLista"] = Autot;
+            List<string> merkit = new List<string>();
+            merkit.Add("Kaikki");
+
+            foreach (Auto auto in autot.Autot)
+            {
+                string merkki = auto.Merkki;
+
+                if (!merkit.Contains(merkki))
+                {
+                    merkit.Add(merkki);    
+                }
+            }
+
+            ListBox1.DataSource = merkit;
+            ListBox1.DataBind();
+            ListBox1.Height = 150; 
+            SortExpression = "DESC";
+            loadListWiew(nelja);
+        }
+        catch (Exception ex)
+        {
+            er.InnerText = ex.Message; 
+        }
+    }
+
+    protected void loadListWiew(List<Auto>a) 
+    {
+        ViewState["currentAutot"] = a; 
+        ListView1.DataSource = a;
+        ListView1.DataBind();
+    }
+
+    public void filterAutot(string merkki) 
+    {
+       List<Auto> results= Autot.FindAll(
+            delegate(Auto a)
+            {
+                return a.Merkki == merkki;
+            });
+
+        loadListWiew(results);
+    }
+
+    protected List<Auto> OrderList(List<Auto> autot) 
+    {
+        if (SortExpression == ("ASC"))
+        {
+            var inOrder = autot.OrderBy(Auto => Auto.MyyntiHinta).ToList<Auto>();
+            autot = inOrder as List<Auto>;
+        }
+        else
+        {
+            var inOrder = autot.OrderByDescending(Auto => Auto.MyyntiHinta).ToList<Auto>();
+            autot = inOrder as List<Auto>;
+        }
+
+        return autot;
+    }
+
+    protected void Page_PreRender(object sender, EventArgs e) 
+    {
+        List<Auto> currentAutot = (List<Auto>)ViewState["currentAutot"];
+
+        if (!string.IsNullOrEmpty(SortExpression))
+        {
+            currentAutot = OrderList(currentAutot);
+        }
+
+        loadListWiew(currentAutot);
+    }
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ListBox1.SelectedIndex > 0)
+        {
+            filterAutot(ListBox1.SelectedValue);
+        }
+        else
+        {
+            loadListWiew(Autot);
+        }
+    }
+
+    protected void ListView1_Sorting(object sender, ListViewSortEventArgs e)
+    {
+        if (SortExpression == ("ASC"))
+        {
+            SortExpression = "DESC";
+        }
+        else
+        {
+            SortExpression = "ASC";
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string haku = TextBox1.Text;
+        List<Auto> results = Autot.FindAll(
+            delegate(Auto a) 
+            { 
+                return a.Merkki.ToLower().Contains(haku.ToLower()) || a.Malli.ToLower().Contains(haku.ToLower()); 
+            }
+        );
+    }
+
+    #region luokat
+    #region serialisointi
+
+
+    public class Serialisointi
+    {
+        public static void DeSerialisoiXml(string filePath, ref AutoLista autoja)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(AutoLista));
+            try
+            {
+                FileStream xmlFile = new FileStream(filePath, FileMode.Open);
+                autoja = (AutoLista)deserializer.Deserialize(xmlFile);
+                xmlFile.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+            }
+        }
+    }
+
+    #endregion
+
+    [Serializable()]
+  	[XmlRoot("Wanhatautot")]
+  	public class AutoLista 
+  	{
+  	    public AutoLista()
+  	    {
+  	        Autot = new List<Auto>();
+  	    }
+
+    [XmlElement("Auto")]
+    public List<Auto> Autot { get; set; }
+
+    }
+
+    [Serializable()]
+    public class Auto
+    {
+        [XmlElement("merkki")]
+        public string Merkki{get;set;}
+        [XmlElement("aid")]
+        public string Aid { get; set; }
+        [XmlElement("rekkari")]
+        public string Rekkari { get; set; }
+        [XmlElement("malli")]
+        public string Malli { get; set; }
+        [XmlElement("vm")]
+        public int Vm { get; set; }
+        [XmlElement("myyntiHinta")]
+        public int MyyntiHinta { get; set; }
+        [XmlElement("sisaanOstoHinta")]
+        public int SOstoHinta { get; set; }
+
+        public Auto()
+        {
+        }
+    } 
+
+    #endregion
+}
