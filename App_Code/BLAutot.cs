d2f6669b2ebe42f836e75f3a58e073fa98081710
vebzical <vebzical@hotmail.com>
2013-11-13 16:52:04 +0200

    Teht 8 valmis
    Tehty tehtävä 8 versio 2 eli md5 +suolaa ja pippuria

--- App_Code/BLAutot.cs
+++ App_Code/BLAutot.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI.WebControls;
@@ -29,7 +31,6 @@ public class BLAutot
         }
         return autoLista;
     }
-
     public static void VieAutot(List<Auto> autoLista) 
     {
         AutoLista autot = new AutoLista();
@@ -41,7 +42,6 @@ public class BLAutot
         
         Serialisointi.SerialisoiXml(HttpContext.Current.Server.MapPath("~/App_Data/WanhatAutot.xml"), autot);
     }
-
     public static List<Auto> SortList(List<Auto> autoLista, string GridViewSortExpression, string SortDirection)
     {
         if (autoLista != null)
@@ -68,7 +68,6 @@ public class BLAutot
             return autoLista;
         }
     }
-
     public static string regexString(string tarkistettava, string kohde) 
     {
         Regex regMerkki = new Regex(@"^[a-zA-Z]{1,15}$");
@@ -78,6 +77,8 @@ public class BLAutot
         Regex regVm = new Regex(@"^[0-9]{4}$");
         Regex regMyyntiHinta = new Regex(@"^[0-9]{1,9}$");
         Regex regSOstoHinta = new Regex(@"^[0-9]{1,9}$");
+        Regex regUserName = new Regex(@"^[a-zA-Z0-9]{1,15}$");
+        Regex regPassword = new Regex(@"^[\S*\s*]{1,20}$");
 
         switch (kohde)
         {
@@ -95,20 +96,30 @@ public class BLAutot
                 return regMyyntiHinta.Match(tarkistettava).ToString();
             case "SOstoHinta":
                 return regSOstoHinta.Match(tarkistettava).ToString();
+            case "userName":
+                return regUserName.Match(tarkistettava).ToString();
+            case "password":
+                return regPassword.Match(tarkistettava).ToString();
             default:
                 return "Tapahtui virhe";
         }
     }
-
     public static bool authenticateUser(string userName, string passWord) 
     {
         Users kayttajat = new Users();
 
         Serialisointi.deSerialisoiKayttajat(HttpContext.Current.Server.MapPath("~/App_Data/Kayttajat.xml"), ref kayttajat);
 
+        byte[] saltBytes = new byte[] { 12, 254, 62, 6, 7, 42, 2, 96 };
+        byte[] saltedHashBytesUserName = new HMACMD5(saltBytes).ComputeHash(Encoding.UTF8.GetBytes(userName));
+        byte[] saltedHashBytesPassword = new HMACMD5(saltBytes).ComputeHash(Encoding.UTF8.GetBytes(passWord));
+
+        string saltedHashStringUserName = Convert.ToBase64String(saltedHashBytesUserName);
+        string saltedHashStringPassword = Convert.ToBase64String(saltedHashBytesPassword);
+
         for (int i = 0; i < kayttajat.kayttajat.Count; i++)
         {
-            if (userName == kayttajat.kayttajat[i].UserName && passWord == kayttajat.kayttajat[i].Password)
+            if (saltedHashStringUserName == kayttajat.kayttajat[i].UserName && saltedHashStringPassword == kayttajat.kayttajat[i].Password)
             {
                 return true;
             }
